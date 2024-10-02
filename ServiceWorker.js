f4c8532d3363e2b6fd746cb6948b5e14d19f2a3b
Messoranze <Messsoranze@gmail.com>
2024-10-02 20:06:40 +0300

    a

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ASME.MEB.HaritaRenklendirme-MEB - Harita Renklendirme-1.0";
+const contentToCache = [
+    "Build/HaritadanPlakalara.loader.js",
+    "Build/HaritadanPlakalara.framework.js",
+    "Build/HaritadanPlakalara.data",
+    "Build/HaritadanPlakalara.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
